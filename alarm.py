765dcdb98dfb2304e0dcad13fc2fe6efc4df0d5a
hwangjaeweoong <jaiuong@kwic.co.kr>
2025-10-02 16:35:41 +0900

    반복요청시 특정오류 발생하는 케이스 수정함

--- alarm.py
+++ alarm.py
@@ -2,14 +2,25 @@ import smtplib
 from email.mime.text import MIMEText
 
 def send_alert(ip, port, result):
+    sender = "scraping1@kwic.co.kr"   # 다우오피스 메일 주소
+    password = "kwic5539!"       # 다우오피스 계정 비번
+    receiver = "scraping1@kwic.co.kr"  # 받는 사람 메일
+
     msg = MIMEText(f"🚨 {ip}:{port} 상태가 {result} 입니다.")
     msg["Subject"] = "포트 점검 알림"
-    msg["From"] = "alert@example.com"
-    msg["To"] = "you@example.com"
+    msg["From"] = sender
+    msg["To"] = receiver
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login("your_gmail@gmail.com", "앱비밀번호")
-        server.send_message(msg)
+    try:
+        # 465 SSL 연결
+        with smtplib.SMTP_SSL("smtp.daouoffice.com", 587) as server:
+            server.ehlo()
+            server.starttls()
+            server.login(sender, password)
+            server.send_message(msg)
+        print(f"✅ 알림 발송 성공: {ip}:{port} → {result}")
 
-    print(f"알림 발송: {ip}:{port} → {result}")
+    except Exception as e:
+        print(f"❌ 메일 발송 실패: {e}")
+if __name__ == "__main__":
+    send_alert('203.243.39.11','80','open')
--- scraper.py
+++ scraper.py
@@ -2,53 +2,60 @@ import asyncio
 import alarm
 from playwright.async_api import async_playwright
 from datetime import datetime
-
 from tuples_list import tuples_list
 
 async def check_port(ip: str, port: str) -> str:
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)  # headless=False면 실제 창 보임
-            # browser = await p.chromium.launch(headless=False, slow_mo=500) # slow_mo: 동작 속도 조절(0.5초)
+            while True:
+                # browser = await p.chromium.launch(headless=False)  # headless=False면 실제 창 보임
+                browser = await p.chromium.launch(headless=False, slow_mo=500) # slow_mo: 동작 속도 조절(0.5초)
 
-            page = await browser.new_page()
-            await page.goto("https://ko.rakko.tools/tools/15/")  # 포트 검사기 메인
+                page = await browser.new_page()
+                await page.goto("https://ko.rakko.tools/tools/15/")  # 포트 검사기 메인
 
-            html = await page.content()
-            if "Begin" in html:
-                print("과부하로 인한 캡차 발생...")
-                while True:
-                    print("10초 대기 후 재시도...")
-                    await page.wait_for_timeout(10000)
-                    html = await page.goto("https://ko.rakko.tools/tools/15/")  # 포트 검사기 메인
+                html = await page.content()
+                if "Begin" in html:
+                    print("과부하로 인한 캡차 발생...")
+                    while True:
+                        print("10초 대기 후 재시도...")
+                        await page.wait_for_timeout(10000)
+                        html = await page.goto("https://ko.rakko.tools/tools/15/")  # 포트 검사기 메인
+                        html = await page.content()
+                        if "검사" in html:
+                            break
+
+                # 입력창 찾기 (보통 첫 번째 input이 IP, 두 번째 input이 포트)
+
+                # IP/Host 입력
+                await page.fill("#textHostOrIp", ip)   # 원하는 IP 또는 호스트
+                # 포트 입력
+                await page.fill("#portNumbers", port)     # 원하는 포트들 (쉼표로 구분 가능)
+                # "검사" 버튼 클릭
+
+                try:
+                    html = await page.click("#jsCheckPort")
+                    print("버튼 클릭 성공")
+                    # 결과 span(#ps_res_1)이 표시될 때까지 대기
+                    await page.wait_for_selector("#ps_res_1", timeout=30000)
                     html = await page.content()
-                    if "검사" in html:
-                        break
+                    with open("결과.txt", "w", encoding="utf-8") as f:
+                        f.write(html)
+                    error = await page.query_selector_all("div.error_area p")
 
-            # 입력창 찾기 (보통 첫 번째 input이 IP, 두 번째 input이 포트)
-            # IP/Host 입력
-            await page.fill("#textHostOrIp", ip)   # 원하는 IP 또는 호스트
+                    if "데이터 수집에 실패했습니다. 입력 내용에 오류가 없는지 확인하십시오." in error:
+                        print("데이터 수집에 실패로 인한 재처리")
+                        await browser.close()
+                        continue
+                    # span의 텍스트 추출
+                    result_text = await page.locator("#ps_res_1").inner_text()
 
-            # 포트 입력
-            await page.fill("#portNumbers", port)     # 원하는 포트들 (쉼표로 구분 가능)
+                    print("검사 결과:", result_text)   # open / closed 등 출력됨
+                except:
+                    await page.locator('button:has-text("검사")').click()
+                    print("버튼 클릭 실패")
 
-            # "검사" 버튼 클릭
-            try:
-                await page.click("#jsCheckPort")
-                print("버튼 클릭 성공")
-            except:
-                await page.locator('button:has-text("검사")').click()
-                print("버튼 클릭 실패")
-
-
-            # 결과 span(#ps_res_1)이 표시될 때까지 대기
-            await page.wait_for_selector("#ps_res_1", timeout=20000)
-
-            # span의 텍스트 추출
-            result_text = await page.locator("#ps_res_1").inner_text()
-            print("검사 결과:", result_text)   # open / closed 등 출력됨
-
-            await browser.close()
-            return result_text
+                await browser.close()
+                return result_text
 
 async def run_checks():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
